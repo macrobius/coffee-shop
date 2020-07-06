d82ce248efe48bce9d19bd8be3f8a7018e007b9e
Joe Attardi <jattardi@gmail.com>
2020-07-06 00:02:30 -0400

    Add menu preview

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,43 +1,32 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import MenuCategory from '../components/MenuCategory';
+import Menu from '../components/Menu';
 
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql } from 'gatsby';
 
-import styles from './menu.module.css';
+export default function MenuPage({ data }) {
+  return (
+    <Layout>
+      <Menu data={data} />
+    </Layout>
+  );
+}
 
-export default function Menu() {
-  const data = useStaticQuery(graphql`
-    {
-      markdownRemark(frontmatter: { contentKey: { eq: "menu" } }) {
-        frontmatter {
-          title
-          categories {
+export const query = graphql`
+  {
+    markdownRemark(frontmatter: { contentKey: { eq: "menu" } }) {
+      frontmatter {
+        title
+        categories {
+          name
+          items {
             name
-            items {
-              name
-              description
-              price
-            }
+            description
+            price
           }
         }
       }
     }
-  `);
-
-  return (
-    <Layout>
-      <div id={styles.main}>
-        <h1>{data.markdownRemark.frontmatter.title}</h1>
-        <div id={styles.menu}>
-          {data.markdownRemark.frontmatter.categories.map(category => (
-            <MenuCategory
-              key={category.name}
-              category={category} />
-          ))}
-        </div>
-      </div>
-    </Layout>
-  );
-}
+  }
+`;
